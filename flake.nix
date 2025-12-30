cdbd0203c6dfc6270089739b5cd2d92b0e74a0cb
MCB-SMART-BOY <mcb2720838051@gmail.com>
2025-12-30 11:35:21 +0800

    chore: update to v0.6.3

--- flake.nix
+++ flake.nix
@@ -14,19 +14,19 @@
         platformMap = {
           "x86_64-linux" = {
             target = "x86_64-unknown-linux-gnu";
-            sha256 = "ab1d70dc486115a2ed2d2c75a2ee8f4085ee94a55f6db6f85e6c4de6086a1c2c";
+            sha256 = "3c43221328bf16afdfcf18e4c0930ebb827adf287ecb614386b1314e7ead6f59";
           };
           "aarch64-linux" = {
             target = "aarch64-unknown-linux-gnu";
-            sha256 = "7bde332766e97198be60eafeb502fc81bf986e4eb5b98af9f84de1ac8ea2f71c";
+            sha256 = "9781b834309e857aee7acbb59c42743c789e8e0c71caf81667971a231cb76066";
           };
           "x86_64-darwin" = {
             target = "x86_64-apple-darwin";
-            sha256 = "ce7d93d8b29b2898bd6958dfc67549ec1ad37c1c054e9844cca4eca34380c053";
+            sha256 = "945adcf93be793aa29d65b1b6b9ebabd9abcaddc5e622deedd21914d432b4dd7";
           };
           "aarch64-darwin" = {
             target = "aarch64-apple-darwin";
-            sha256 = "1be1ff1446f6e18b2ebbb10c11a7d1e5ec6e14086db36af4ede461a6207e6bde";
+            sha256 = "b4ddf9dd5f0cf251172a5c2b38e98c07069f8ca3200aefa7dbfe4d783ac15bc1";
           };
         };
 
@@ -35,7 +35,7 @@
       {
         packages.default = pkgs.stdenv.mkDerivation rec {
           pname = "neve";
-          version = "0.6.2";
+          version = "0.6.3";
 
           src = pkgs.fetchurl {
             url = "https://github.com/MCB-SMART-BOY/Neve/releases/download/v${version}/neve-${platformInfo.target}.tar.gz";
