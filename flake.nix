6d4f435c6d15f2d2420790ac01f1afa5f8b6de9a
MCB-SMART-BOY <2720838051@qq.com>
2026-05-07 02:32:29 +0800

    fix SHA256

--- flake.nix
+++ flake.nix
@@ -14,19 +14,19 @@
         platformMap = {
           "x86_64-linux" = {
             target = "x86_64-unknown-linux-gnu";
-            sha256 = "b9a18654b1533e8108b730ed53cda537c266fe5eaf9613308a3fbd817643954c";
+            sha256 = "df8021e53846ec76c06b0539e0ab6bcdf0a0366b2070deae6ec362a8c78169e9";
           };
           "aarch64-linux" = {
             target = "aarch64-unknown-linux-gnu";
-            sha256 = "edf12e197fa1de7b08a2ec9f76ab536a7dff3276442e55f27929d4e54fce9424";
+            sha256 = "df8021e53846ec76c06b0539e0ab6bcdf0a0366b2070deae6ec362a8c78169e9";
           };
           "x86_64-darwin" = {
             target = "x86_64-apple-darwin";
-            sha256 = "d2053860833cbe9394a03a50f81058519ccf4163ade24c49eb280f6b786bfcf3";
+            sha256 = "df8021e53846ec76c06b0539e0ab6bcdf0a0366b2070deae6ec362a8c78169e9";
           };
           "aarch64-darwin" = {
             target = "aarch64-apple-darwin";
-            sha256 = "01f198f04f5f9d5ac9e7006800e556a1096c41ad7e8895a817014c3103493b38";
+            sha256 = "df8021e53846ec76c06b0539e0ab6bcdf0a0366b2070deae6ec362a8c78169e9";
           };
         };
 
